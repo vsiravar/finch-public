1818e5d6e2d3843114347f2ff127105cf1944b16
Vishwas Siravara <vsiravara@gmail.com>
2023-03-20 18:09:57 -0700

    Update main.go

--- cmd/finch/main.go
+++ cmd/finch/main.go
@@ -37,7 +37,7 @@ func main() {
 	if err := xmain(logger, stdLib, fs, stdLib, mem, stdOut); err != nil {
 		logger.Fatal(err)
 	}
-	println("Break")
+	println("Break feature")
 }
 
 func xmain(logger flog.Logger,
